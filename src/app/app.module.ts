b0685fc59672a5cdef27e4d03875f91ba9babb15
Thanos Sklavos <thanos.sklavos@outlook.com>
2023-04-02 00:59:02 +0300

    commit v0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyCompComponent } from './my-comp/my-comp.component';
+import { Child1Component } from './my-comp/child1/child1.component';
+import { Child2Component } from './my-comp/child2/child2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCompComponent,
+    Child1Component,
+    Child2Component
   ],
   imports: [
     BrowserModule
